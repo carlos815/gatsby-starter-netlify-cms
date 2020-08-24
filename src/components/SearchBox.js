b950e2543b7090bece58cfe1ee0c24b8780f3bf9
Carlos Hernández <carlos_t815@outlook.com>
2020-08-24 18:34:17 -0400

    Edited teh search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,24 +6,24 @@ const SearchBox = ({ data }) => {
   return (
     <div className="search-box bg-background-primary-color flex flex-col rounded-2xl justify-between items-stretch p-10 max-w-lg">
       <div className="input-group first">
-        <label className="label">Where you go?</label>
+        <label className="label font-bold">Where do you want to go?</label>
 
         <input className="input input-location" type="text" placeholder='try "Singapore"' />
       </div>
 
       <div className="grid grid-cols-2 gap-6">
         <div className="input-group flex-1">
-          <label className="label">Check in</label>
+          <label className="label font-bold">Check in</label>
           <input className="input" type="date" />
         </div>
         <div className="input-group flex-1">
-          <label className="label">Check out</label>
+          <label className="label font-bold">Check out</label>
           <input className="input" type="date" />
         </div>
       </div>
 
       <div className="input-group">
-        <label className="label">Guest</label>
+        <label className="label font-bold">Guest</label>
         <select id="guests" name="guests" className="input">
           <option defaultValue disabled selected>
             How many guests?
@@ -38,7 +38,7 @@ const SearchBox = ({ data }) => {
 
       <div className="checkgroup relative flex items-center gap-3 pb-3 z-10">
         <input className='checkmark'type="checkbox" id="payAfter" name="payAfter" value="" />
-        <label className='font-bold' htmlFor="payAfter">Pay when checking in?</label>
+        <label className='' htmlFor="payAfter">Pay when checking in?</label>
       </div>
 
       <button formAction="submit" className="btn">
